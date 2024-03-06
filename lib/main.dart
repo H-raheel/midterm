a604900af7e726dbbad6de7c6d826f6f40f32776
H-raheel <h.raheel622@gmail.com>
2024-03-06 16:56:45 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -130,10 +130,18 @@ class _UsersScreenState extends State<UsersScreen> {
                                               onPressed: () {
                                                 print("here");
                                               },
-                                              child: Text("More",
-                                                  style: TextStyle(
-                                                      color: Colors.blue,
-                                                      fontSize: 15)),
+                                              child: Row(
+                                                children: [
+                                                  Text("More",
+                                                      style: TextStyle(
+                                                          color: Colors.blue,
+                                                          fontSize: 15)),
+                                                  Icon(
+                                                    Icons.arrow_downward,
+                                                    color: Colors.blue,
+                                                  )
+                                                ],
+                                              ),
                                               style: ElevatedButton.styleFrom(
                                                   shape: RoundedRectangleBorder(
                                                     borderRadius:
@@ -184,195 +192,7 @@ class _UsersScreenState extends State<UsersScreen> {
                 return const CircularProgressIndicator();
             },
           ),
-        )
-
-        // SafeArea(
-        //     child: FutureBuilder(
-        //   future: futureUsersList,
-        //   builder: (context, snapshot) {
-        //     if (snapshot.hasData) {
-        //       return ListView.builder(
-        //           itemCount: snapshot.data?.length,
-        //           itemBuilder: (context, index) {
-        //             var user = snapshot.data![index];
-        //             return AspectRatio(
-        //               aspectRatio: 4 / 2.5,
-        //               // padding: const EdgeInsets.symmetric(vertical: 10),
-        //               child: Padding(
-        //                   padding: const EdgeInsets.fromLTRB(4, 10, 4, 10),
-        //                   child: Card(
-
-        //                       //     child: ListTile(
-        //                       //   // leading: CircleAvatar(child: Image.network("hh")),
-        //                       //   title: Text(index.toString()),
-        //                       //   subtitle: Text(user.title),
-        //                       //   trailing: Icon(Icons.visibility),
-        //                       //   onTap: () {
-        //                       //     showModalBottomSheet(
-        //                       //         context: context,
-        //                       //         builder: (context) {
-        //                       //           return Container(
-        //                       //               height: MediaQuery.of(context).size.width,
-        //                       //               width: MediaQuery.of(context).size.width,
-        //                       //               child: Center(
-        //                       //                 child: Text(user.title),
-        //                       //               ));
-        //                       //         });
-        //                       //   },
-        //                       // )
-        //                       clipBehavior: Clip.antiAlias,
-        //                       elevation: 3,
-        //                       shape: const RoundedRectangleBorder(
-        //                           borderRadius:
-        //                               BorderRadius.all(Radius.circular(20))),
-        //                       // child: SizedBox(
-        //                       // height: 260,
-        //                       // width: 600,
-        //                       child: Column(
-        //                         // mainAxisAlignment:
-        //                         //     MainAxisAlignment.spaceBetween,
-        //                         crossAxisAlignment: CrossAxisAlignment.start,
-        //                         children: <Widget>[
-        //                           // AspectRatio(
-        //                           //   aspectRatio: 18.0 / 11.0,
-        //                           //   child: Image.network(user.thumbnail),
-        //                           // ),
-        //                           AspectRatio(
-        //                             aspectRatio: 4 / 1,
-        //                             child: Image.network(user.thumbnail,
-        //                                 // height: 100,
-        //                                 // width: 600,
-        //                                 fit: BoxFit.cover),
-        //                           ),
-        //                           //  SizedBox(height: 20),
-        //                           Padding(
-        //                               padding:
-        //                                   const EdgeInsets.fromLTRB(5, 0, 5, 0),
-        //                               child: Row(
-        //                                 children: [
-        //                                   Text(
-        //                                     user.title.length > 20
-        //                                         ? user.title.substring(0, 20)
-        //                                         : user.title,
-        //                                     maxLines: 1,
-        //                                     style: const TextStyle(
-        //                                         fontWeight: FontWeight.bold,
-        //                                         fontSize: 18),
-        //                                   ),
-        //                                   const Spacer(),
-        //                                   Row(children: [
-        //                                     Text(
-        //                                       user.price.toString() + "USD",
-        //                                       maxLines: 1,
-        //                                       style: const TextStyle(
-        //                                           fontWeight: FontWeight.bold,
-        //                                           fontSize: 18),
-        //                                     ),
-        //                                     const SizedBox(width: 10),
-        //                                     IconButton(
-        //                                         onPressed: () {
-        //                                           showBottomSheet(
-        //                                               elevation: 20,
-        //                                               context: context,
-        //                                               builder: (context) {
-        //                                                 APIDataModel prod =
-        //                                                     user;
-        //                                                 return Container(
-        //                                                   height: 0.5 *
-        //                                                       MediaQuery.of(
-        //                                                               context)
-        //                                                           .size
-        //                                                           .height,
-        //                                                   child: Column(
-        //                                                     mainAxisAlignment:
-        //                                                         MainAxisAlignment
-        //                                                             .spaceAround,
-        //                                                     mainAxisSize:
-        //                                                         MainAxisSize
-        //                                                             .min,
-        //                                                     children: [
-        //                                                       AspectRatio(
-        //                                                         aspectRatio:
-        //                                                             3 / 1,
-        //                                                         child: Expanded(
-        //                                                           child: ListView.builder(
-        //                                                               scrollDirection: Axis.horizontal,
-        //                                                               // shrinkWrap:
-        //                                                               //     true,
-        //                                                               itemCount: prod.images.length,
-        //                                                               itemBuilder: (context, index) {
-        //                                                                 return Padding(
-        //                                                                   padding: const EdgeInsets
-        //                                                                       .symmetric(
-        //                                                                       horizontal: 8),
-        //                                                                   child:
-        //                                                                       Image.network(prod.images[index]),
-        //                                                                 );
-        //                                                               }),
-        //                                                         ),
-        //                                                       ),
-        //                                                       Column(
-        //                                                         children: [
-        //                                                           Text(prod
-        //                                                               .title),
-        //                                                           Text(prod
-        //                                                               .description),
-        //                                                           Text(
-        //                                                               "\$ ${prod.price}"),
-        //                                                           Row(
-        //                                                             mainAxisAlignment:
-        //                                                                 MainAxisAlignment
-        //                                                                     .spaceBetween,
-        //                                                             children: [
-        //                                                               const Row(
-        //                                                                 children: [
-        //                                                                   Icon(Icons
-        //                                                                       .star)
-        //                                                                 ],
-        //                                                               ),
-        //                                                               Row(
-        //                                                                 children: [
-        //                                                                   Text(
-        //                                                                       "${prod.discountPercentage} %"),
-        //                                                                   const Icon(
-        //                                                                     Icons.discount,
-        //                                                                   )
-        //                                                                 ],
-        //                                                               ),
-        //                                                             ],
-        //                                                           )
-        //                                                         ],
-        //                                                       ),
-        //                                                     ],
-        //                                                   ),
-        //                                                 );
-        //                                               });
-        //                                         },
-        //                                         icon: const Icon(
-        //                                             Icons.remove_red_eye_sharp))
-        //                                   ])
-        //                                 ],
-        //                               )),
-        //                           //  const Spacer(),
-
-        //                           Text(
-        //                             user.description,
-        //                             overflow: TextOverflow.ellipsis,
-        //                             maxLines: 2,
-        //                           ),
-        //                         ],
-        //                       ))),
-        //             );
-        //           });
-        //     } else if (snapshot.hasError) {
-        //       return Text(snapshot.error.toString());
-        //     }
-        //     return const CircularProgressIndicator();
-        //   },
-        // ) // This trailing comma makes auto-formatting nicer for build methods.
-        //     )
-
-        );
+        ));
   }
 
   Widget buildItemWidget(String item) {
